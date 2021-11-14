abe88ee6d1eefe66f763f8f45b1e860a49c0a689
sheersh gaur <73462583+sheersh02@users.noreply.github.com>
2021-11-14 23:50:00 +0530

    Add files via upload

---
+++ NumSwap.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class NumSwap
+{
+    public static void main(String args[])
+    {
+    int num1, num2, temp ;
+    
+    Scanner scan =new Scanner(System.in);
+    
+    System.out.println("enter marks of first number");
+    num1=scan.nextInt();
+    
+    System.out.println("enter marks of second number");
+    num2=scan.nextInt();
+    
+    temp=num1;
+    num1=num2;
+    num2=temp;
+    System.out.println("The swapped number1"+num1);
+    System.out.println("The swaped number"+num2);
+    }
+}
