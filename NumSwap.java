98a340f08ddf92d5a352381ec53a7be4b6e44ad1
sheersh gaur <sheersh.2@gmail.com>
2021-11-25 22:59:39 +0530

    Update NumSwap.java

--- NumSwap.java
+++ NumSwap.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
-class NumSwap
-{
-    public static void main(String args[])
-    {
-    int num1, num2, temp ;
-    
-    Scanner scan =new Scanner(System.in);
-    
-    System.out.println("enter marks of first number");
-    num1=scan.nextInt();
-    
-    System.out.println("enter marks of second number");
-    num2=scan.nextInt();
-    
-    temp=num1;
-    num1=num2;
-    num2=temp;
-    System.out.println("The swapped number1"+num1);
-    System.out.println("The swaped number"+num2);
+class NumSwap {
+    public static void main(String args[]) {
+        int num1, num2, temp;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("enter marks of first number");
+        num1 = scan.nextInt();
+
+        System.out.println("enter marks of second number");
+        num2 = scan.nextInt();
+
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("The swapped number  " + num1);
+        System.out.println("The swaped number  " + num2);
     }
 }
